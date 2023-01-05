e2c87dc8e46d125c70f14c3f27968739fd7eff94
Reginald Wong <reginald_wong@outlook.com>
2023-01-05 08:51:01 -0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note App Footer, 2022</em>
+    </div>
+  );
+};
+
+export default Footer;
